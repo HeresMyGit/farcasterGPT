1b00d13f30074e5c4e1732d5dd3ca441d34feb92
HeresMyGit <100598193+HeresMyGit@users.noreply.github.com>
2024-12-03 17:08:58 -0800

    Update crons for paid version

--- tweet.js
+++ tweet.js
@@ -535,7 +535,7 @@ const topics = ["mfers", "mfercoin", "mfers", "$mfer", "mfers nfts", "ai", "onch
 
 
 // Runs every 4 hours starting at midnight (Pacific Time)
-cron.schedule('0 0-23/4 * * *', async () => {
+cron.schedule('0 0-23/1 * * *', async () => {
   console.log('Running the scheduled tweetAssistantResponse...');
   const randomLength = lengths[Math.floor(Math.random() * lengths.length)];
   const prompt = `the next random tweet should be ${randomLength}.  remember always include $mfer. next`;
@@ -550,7 +550,7 @@ cron.schedule('30 7 * * *', async () => {
 });
 
 // Runs every day at 5pm Pacific Time
-cron.schedule('0 17 * * *', async () => {
+cron.schedule('0 1,5,11,17 * * *', async () => {
   console.log('Running scheduled task to fetch and post the most popular $mfer tweet...');
   await postMostPopularMferTweet();
 });
@@ -562,8 +562,8 @@ cron.schedule('30 6,18 * * *', async () => {
 });
 
 // Runs every 4 hours starting at midnight (Pacific Time)
-cron.schedule('0 2,6,10,14,18,22 * * *', async () => {
-  console.log('Running reply to recent mention')
+cron.schedule('* * * * *', async () => {
+  console.log('Running reply to recent mention every minute');
   const USER_ID = '1724482668195110912'; // Replace with your actual user ID
   await fetchAndReplyToMostLikedMention(USER_ID);
 });
@@ -572,7 +572,7 @@ cron.schedule('0 2,6,10,14,18,22 * * *', async () => {
 //   await sendDailyGMTweet();
 // })();
 
-(async () => {
-  const USER_ID = '1724482668195110912'; // Replace with your actual user ID
-  await fetchAndReplyToMostLikedMention(USER_ID);
-})();
+// (async () => {
+//   const USER_ID = '1724482668195110912'; // Replace with your actual user ID
+//   await fetchAndReplyToMostLikedMention(USER_ID);
+// })();
