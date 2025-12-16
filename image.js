2f16acc15e2ad07ca6c0370bd3e5ecfea7c838c2
HeresMyGit <100598193+HeresMyGit@users.noreply.github.com>
2025-12-16 10:19:48 -0800

    update to gpt-image-1.5

--- image.js
+++ image.js
@@ -11,7 +11,7 @@ async function generateImage(prompt) {
       prompt: prompt,
       n: 1,
       size: "1024x1024",
-      model: "gpt-image-1",
+      model: "gpt-image-1.5",
     });
 
     const imageBase64 = imageResponse.data[0].b64_json;
